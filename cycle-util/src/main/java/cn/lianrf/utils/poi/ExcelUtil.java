ddd2d9f14b6415dee6f5da83f12c8e9c1fec1983
rongfa.lian@drcnet.com.cn <rongfa.lian@drcnet.com.cn>
2020-10-20 10:25:09 +0800

    mongo index copy

--- cycle-util/src/main/java/cn/lianrf/utils/poi/ExcelUtil.java
+++ cycle-util/src/main/java/cn/lianrf/utils/poi/ExcelUtil.java
@@ -1,8 +1,10 @@
 package cn.lianrf.utils.poi;
 
-import org.apache.poi.hssf.usermodel.*;
-import org.apache.poi.ss.usermodel.HorizontalAlignment;
-
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.ss.util.NumberToTextConverter;
+import org.springframework.util.CollectionUtils;
+import org.springframework.util.StringUtils;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.OutputStream;
@@ -10,6 +12,7 @@ import java.lang.reflect.Field;
 import java.net.URLEncoder;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -17,11 +20,13 @@ import java.util.Optional;
  * excel下载导出工具包
  *
  * excel直接导出，使用：
- * {@link #exportAction(HSSFWorkbook, HttpServletResponse, String)}
+ * {@link #exportAction(Workbook, HttpServletResponse, String)}
  *
  * 数据填充excel导出，使用：
  * {@link #export(List, String, HttpServletResponse)}
  *
+ * 追加数据到单一sheet导出Excel文件
+ * {@link #export(List, Workbook, String)}
  */
 public abstract class ExcelUtil {
 
@@ -31,10 +36,10 @@ public abstract class ExcelUtil {
      * @param fileName 要保存的文件名称
      * @param response
      */
-    public static void exportAction(HSSFWorkbook workbook, HttpServletResponse response, String fileName) {
+    public static void exportAction(Workbook workbook, HttpServletResponse response, String fileName) {
 
         try {
-            setResponseHeader(response, fileName + ".xls");
+            setResponseHeader(response, fileName + ".xlsx");
             OutputStream os = response.getOutputStream();
             workbook.write(os);
             os.flush();
@@ -42,6 +47,21 @@ public abstract class ExcelUtil {
         } catch (Exception e) {
             throw new RuntimeException("服务器异常");
         }
+    }
+
+    /**
+     * 追加数据到单一sheet导出Excel文件
+     *
+     * @param data     数据列表
+     * @param <C>      必须是泛型列表
+     */
+    public static <C> void export(List<C> data, Workbook workbook, String sheetName) {
+        if(CollectionUtils.isEmpty(data)){
+            return;
+        }
+        String[] titles = generateTitle(data.get(0).getClass());
+        Object[][] values = generateValue(data);
+        getHSSFWorkbook(sheetName, titles, values, workbook);
     }
 
     /**
@@ -55,20 +75,38 @@ public abstract class ExcelUtil {
     public static <C> void export(List<C> data, String fileName, HttpServletResponse response) {
         if (data == null || data.isEmpty()) {
             try {
-                response.getWriter().println("数据为空");
+                response.getWriter().println("data empty");
             } catch (IOException e) {
 
             }
             return;
         }
         String[] titles = generateTitle(data.get(0).getClass());
-        String[][] values = generateValue(data);
+        Object[][] values = generateValue(data);
 
         if (fileName == null)
             fileName = "数据" + LocalDateTime.now().getSecond();
-        HSSFWorkbook workbook = getHSSFWorkbook(fileName, titles, values);
+        Workbook workbook = getHSSFWorkbook(fileName, titles, values);
         exportAction(workbook, response, fileName);
     }
+
+    public static <C> void export(List<C> data, String fileName, Workbook workbook, HttpServletResponse response) {
+        if (data == null || data.isEmpty()) {
+            try {
+                response.getWriter().println("data empty");
+            } catch (IOException e) {
+
+            }
+            return;
+        }
+        Object[][] values = generateValue(data);
+
+        if (fileName == null)
+            fileName = "数据" + LocalDateTime.now().getSecond();
+        billSheet(values,workbook);
+        exportAction(workbook, response, fileName);
+    }
+
 
     //设置响应头
     private static void setResponseHeader(HttpServletResponse response, String fileName) {
@@ -106,32 +144,32 @@ public abstract class ExcelUtil {
      *
      * @param data 查询条件参数
      */
-    private static <C> String[][] generateValue(List<C> data) {
+    private static <C> Object[][] generateValue(List<C> data) {
         if (data == null || data.isEmpty())
             return new String[0][];
         Class<?> aClass = data.get(0).getClass();
         int size = data.size();
-        String[][] values = new String[size][];
+        Object[][] values = new Object[size][];
 
         for (int i = 0; i < size; i++) {
             Field[] fields = aClass.getDeclaredFields();
-            List<String> list = new ArrayList<>();
+            List<Object> list = new ArrayList<>();
             for (Field field : fields) {
                 if (field.getAnnotation(ColumnName.class) != null) {
                     field.setAccessible(true);
                     try {
-                        list.add(String.valueOf(Optional.ofNullable(field.get(data.get(i))).orElse("")));
+                        list.add(Optional.ofNullable(field.get(data.get(i))).orElse(""));
                     } catch (IllegalAccessException e) {
                         e.printStackTrace();
                     }
                 }
             }
-            values[i] = list.toArray(new String[0]);
+            values[i] = list.toArray();
         }
         return values;
     }
 
-    public static HSSFWorkbook getHSSFWorkbook(String sheetName, String[] title, String[][] values) {
+    public static Workbook getHSSFWorkbook(String sheetName, String[] title, Object[][] values) {
         return getHSSFWorkbook(sheetName, title, values, null);
     }
 
@@ -144,7 +182,7 @@ public abstract class ExcelUtil {
      * @param wb        HSSFWorkbook对象
      * @return
      */
-    public static HSSFWorkbook getHSSFWorkbook(String sheetName, String[] title, String[][] values, HSSFWorkbook wb) {
+    public static Workbook getHSSFWorkbook(String sheetName, String[] title, Object[][] values, Workbook wb) {
 
         // 第一步，创建一个HSSFWorkbook，对应一个Excel文件
         if (wb == null) {
@@ -152,32 +190,90 @@ public abstract class ExcelUtil {
         }
 
         // 第二步，在workbook中添加一个sheet,对应Excel文件中的sheet
-        HSSFSheet sheet = wb.createSheet(sheetName);
+        Sheet sheet = wb.getSheet(sheetName);
+        if(sheet==null){
+            sheet = wb.createSheet(sheetName);
+        }
 
         // 第三步，在sheet中添加表头第0行,注意老版本poi对Excel的行数列数有限制
-        HSSFRow row = sheet.createRow(0);
-
-        // 第四步，创建单元格，并设置值表头 设置表头居中
-        HSSFCellStyle style = wb.createCellStyle();
-        style.setAlignment(HorizontalAlignment.CENTER); // 创建一个居中格式
-
-        //声明列对象
-        HSSFCell cell = null;
-
-        //创建标题
-        for (int i = 0; i < title.length; i++) {
-            cell = row.createCell(i);
-            cell.setCellValue(title[i]);
-            cell.setCellStyle(style);
+        Row row=null;
+        int lastRowNum = sheet.getLastRowNum();
+        if (lastRowNum == 0) {
+            row = sheet.createRow(0);
+            // 第四步，创建单元格，并设置值表头 设置表头居中
+            CellStyle style = wb.createCellStyle();
+            style.setAlignment(HorizontalAlignment.CENTER); // 创建一个居中格式
+            //声明列对象
+            Cell cell = null;
+            for (int i = 0; i < title.length; i++) {
+                cell = row.createCell(i);
+                cell.setCellValue(title[i]);
+                cell.setCellStyle(style);
+            }
         }
+
         //创建内容
         for (int i = 0; i < values.length; i++) {
-            row = sheet.createRow(i + 1);
+            row = sheet.createRow(++lastRowNum);
             for (int j = 0; j < values[i].length; j++) {
                 //将内容按顺序赋给对应的列对象
-                row.createCell(j).setCellValue(values[i][j]);
+                setCellValue(row.createCell(j),values[i][j]);
             }
         }
         return wb;
+    }
+
+    public static void setCellValue(Cell cell, Object value){
+        if(value instanceof String){
+            cell.setCellValue((String)value);
+        }else if(value instanceof Date){
+            cell.setCellValue((Date)value);
+        }else if(value instanceof Double){
+            cell.setCellValue((Double)value);
+        }else if(value instanceof Boolean){
+            cell.setCellValue((Boolean)value);
+        }else if(value instanceof Integer){
+            cell.setCellValue((Integer)value);
+        }
+    }
+
+    public static void billSheet(Object[][] values, Workbook wb) {
+
+        Sheet sheet = wb.getSheetAt(0);
+
+        //创建单元格，并设置值表头 设置表头居中
+        CellStyle style = wb.createCellStyle();
+        style.setAlignment(HorizontalAlignment.CENTER); // 创建一个居中格式
+
+
+        //创建内容
+        for (int i = 0; i < values.length; i++) {
+            Row row = sheet.createRow(i + 1);
+            for (int j = 0; j < values[i].length; j++) {
+                //将内容按顺序赋给对应的列对象
+                setCellValue(row.createCell(j),values[i][j]);
+            }
+        }
+    }
+
+
+    public static String getStringCellValue(Cell cell) {
+        if (cell == null
+                || (cell.getCellTypeEnum() == CellType.STRING && StringUtils.isEmpty(cell.getStringCellValue()))) {
+            return null;
+        }
+        CellType cellType = cell.getCellTypeEnum();
+        switch (cellType) {
+            case BLANK:
+                return null;
+            case BOOLEAN:
+                return String.valueOf(cell.getBooleanCellValue());
+            case NUMERIC:
+                return NumberToTextConverter.toText(cell.getNumericCellValue());
+            case STRING:
+                return cell.getStringCellValue();
+            default:
+                return null;
+        }
     }
 }
